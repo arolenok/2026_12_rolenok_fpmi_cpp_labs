4d3aa013627db54748e76f073a4920693bc3927f
Александр Р <fpm.rolenok@bsu.by>
2026-09-11 13:32:24 +0300

    lab2_var7 done

--- semester_1/lab2_integer_arithmetics/lab2_var7.cpp
+++ semester_1/lab2_integer_arithmetics/lab2_var7.cpp
@@ -2,15 +2,27 @@
 #include <cmath>
 
 int main() {
-    int n;
-    std::cin >> n;
-    for (int a = 1; a <= n; ++a ) {
-        for (int b = a; b <= n; ++b ) {
-            int c = std::lround(std::sqrt(a*a + b*b));
-            if (a*a + b*b == c*c && c <= n) {
-                std::cout << a << " " << b << " " << c << std::endl;
+    int n, c, squares_sum;
+    bool flag = false;
+    std:: cout << "enter an integer: ";
+    if (std::cin >> n and n > 0) {
+        std::cout << "pythagorean triples for n = " << n << ":" << std::endl;
+        for (int a = 1; a<= n; a++) {
+            for (int b = a; b <= n; b++) {
+                squares_sum = a * a + b * b;
+                c = std::sqrt(squares_sum);
+                if (c <= n and c * c == squares_sum) {
+                    std::cout << "a = " << a << ", " << "b = " << b << ", " << "c = " << c << std::endl;
+                    flag = true;
+                    }
+                }
             }
+        if (!flag) {
+            std::cout << "no pythagorean triples found." << std::endl;
         }
+        return 0;
+    } else {
+        std::cout << "invalid input. try again. possible errors: negative numbers, zero, or non-integer values." << std::endl;
+        return 1;
     }
-    return 0;
 }
