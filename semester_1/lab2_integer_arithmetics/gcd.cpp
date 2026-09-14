7d34f55e381f797dffb0e670bbde976b08d48414
Александр Р <fpm.rolenok@bsu.by>
2026-09-14 10:48:11 +0300

    gcd fixed

--- semester_1/lab2_integer_arithmetics/gcd.cpp
+++ semester_1/lab2_integer_arithmetics/gcd.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <numeric>
+#include <algorithm>
+#include <cstdlib>
 
 int Gcd(int a, int b) {
-    while (a * b) {
+    a = std::abs(a);
+    b = std::abs(b);
+    while (b != 0) {
         int rem = a % b;
         a = b;
         b = rem;
     }
-    return std::max(a, b);
+    return a;
 }
 
 void TryRead(int& number) {
     if (!(std::cin >> number)) {
         std::cout << "Fail on reading the number" << std::endl;
-        std::exit(0);
+        std::exit(1);
     }
 }
 
@@ -26,19 +30,13 @@ int main() {
     std::cout << "Enter b: ";
     TryRead(b);
     
-    if (a <= 0 || b <= 0) {
-        std::cout << "Numbers must be positive" << std::endl;
-        return 0;
-    }
-
     int gcd1 = Gcd(a, b);
     int gcd2 = std::gcd(a, b);
     if (gcd1 != gcd2) {
         std::cout << "You broke my program :(" << std::endl;
         return 1;
-    } else {
-        std::cout << "Try again..." << std::endl;
     }
+    std::cout << "GCD is: " << gcd1 << std::endl;
 
     return 0;
 }
